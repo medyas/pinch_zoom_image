5d806c2c8b8df913808b04130e66bd311a496242
medyas <medyassinesabri@gmail.com>
2020-10-10 16:06:39 +0100

    fixed minor bug

--- android/src/main/kotlin/com/shatsy/pinchzoomimage/PinchZoomImagePlugin.kt
+++ android/src/main/kotlin/com/shatsy/pinchzoomimage/PinchZoomImagePlugin.kt
@@ -1,6 +1,5 @@
 package com.shatsy.pinchzoomimage
 
-import android.app.Activity
 import android.view.View
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -9,28 +8,28 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class PinchZoomImagePlugin(private val activity: Activity) : MethodCallHandler {
-    private val originalSystemUiVisibility = activity?.window?.decorView?.systemUiVisibility!!
+class PinchZoomImagePlugin(private val registrar: Registrar) : MethodCallHandler {
+    private val originalSystemUiVisibility = registrar.activity()?.window?.decorView?.systemUiVisibility!!
 
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "pinch_zoom_image")
-            channel.setMethodCallHandler(PinchZoomImagePlugin(registrar.activity()))
+            channel.setMethodCallHandler(PinchZoomImagePlugin(registrar))
         }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             "hideStatusBar" -> {
-                activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility or
+                registrar.activity().window?.decorView?.systemUiVisibility = originalSystemUiVisibility or
                         View.SYSTEM_UI_FLAG_FULLSCREEN and
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN.inv() and
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE.inv()
                 result.success(null)
             }
             "showStatusBar" -> {
-                activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility
+                registrar.activity().window?.decorView?.systemUiVisibility = originalSystemUiVisibility
                 result.success(null)
             }
             else -> result.notImplemented()
