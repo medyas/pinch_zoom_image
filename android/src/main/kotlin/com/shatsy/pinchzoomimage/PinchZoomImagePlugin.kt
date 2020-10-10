8e1fdb114a99069d1226abab879ae34025d23fc6
medyas <medyassinesabri@gmail.com>
2020-10-10 14:56:05 +0100

    fixed a bug

--- android/src/main/kotlin/com/shatsy/pinchzoomimage/PinchZoomImagePlugin.kt
+++ android/src/main/kotlin/com/shatsy/pinchzoomimage/PinchZoomImagePlugin.kt
@@ -2,14 +2,16 @@ package com.shatsy.pinchzoomimage
 
 import android.app.Activity
 import android.view.View
+
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class PinchZoomImagePlugin(private val activity: Activity) : MethodCallHandler {
-  private val originalSystemUiVisibility = activity.window?.decorView?.systemUiVisibility!!
+class PinchZoomImagePlugin(private val registrar: Registrar) : MethodCallHandler {
+  private val originalSystemUiVisibility = registrar.activity.window?.decorView?.systemUiVisibility!!
 
   companion object {
     @JvmStatic
@@ -20,19 +22,19 @@ class PinchZoomImagePlugin(private val activity: Activity) : MethodCallHandler {
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    when {
-      call.method == "hideStatusBar" -> {
-        activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility or
-          View.SYSTEM_UI_FLAG_FULLSCREEN and
-          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN.inv() and
-          View.SYSTEM_UI_FLAG_LAYOUT_STABLE.inv()
-        result.success(null)
-      }
-      call.method == "showStatusBar" -> {
-        activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility
-        result.success(null)
-      }
-      else -> result.notImplemented()
+    when (call.method) {
+        "hideStatusBar" -> {
+          registrar.activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility or
+                  View.SYSTEM_UI_FLAG_FULLSCREEN and
+                  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN.inv() and
+                  View.SYSTEM_UI_FLAG_LAYOUT_STABLE.inv()
+          result.success(null)
+        }
+        "showStatusBar" -> {
+          registrar.activity.window?.decorView?.systemUiVisibility = originalSystemUiVisibility
+          result.success(null)
+        }
+        else -> result.notImplemented()
     }
   }
 }
